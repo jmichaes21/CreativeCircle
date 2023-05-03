5fdc570f8705b81883099b97c27f37185023da56
jmichaes21 <juansalainti@gmail.com>
2023-05-03 20:32:04 +0800

    initialize CreativeCircle Apps

---
+++ CreativeCircle/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import SignIn from './src/screens/SignIn/index.js';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>CreativeCircle</Text>
+    </View>
+  );
+};
+
+//Function Component
+
+//Export
+export default App;
