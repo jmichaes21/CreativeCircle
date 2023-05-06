33144342176171454f50325e8b1233bbdd4604b5
ManahaCheisyaFL <s11920019@student.unklab.ac.id>
2023-05-06 20:43:04 +0800

    adding OpenApp screen

--- CreativeCircle/App.js
+++ CreativeCircle/App.js
@@ -4,15 +4,12 @@ import {View, Text} from 'react-native';
 import Button from './source/components/atoms/Button';
 import TextInput from './source/components/atoms/TextInput';
 import Header from './source/components/molecules/Header';
+import OpenApp from './source/screens/OpenApp';
 
 const App = () => {
   return (
     <View>
-      <Header>
-        <Text>Halo</Text>
-      </Header>
-      <Button title="Testing Button" color="#65BC9A" />
-      <TextInput title="Email Address" placeholder="Insert Your Email" />
+      <OpenApp />
     </View>
   );
 };
