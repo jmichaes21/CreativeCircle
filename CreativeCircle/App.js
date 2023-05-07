a28b457f5b2148a372618a29aeff5a0f7409d455
unknown <s2200042@student.unklab.ac.id>
2023-05-07 21:13:26 +0800

    adding ContactDesigner screen

--- CreativeCircle/App.js
+++ CreativeCircle/App.js
@@ -1,23 +1,16 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 // import SignIn from './src/screens/SignIn/index.js';
 import {View, Text} from 'react-native';
-import Button from './source/components/atoms/Button';
-import TextInput from './source/components/atoms/TextInput';
-import Header from './source/components/molecules/Header';
+import OpenApp from './source/screens/OpenApp';
 
 const App = () => {
   return (
-    <View>
-      <Header>
-        <Text>Halo</Text>
-      </Header>
-      <Button title="Testing Button" color="#65BC9A" />
-      <TextInput title="Email Address" placeholder="Insert Your Email" />
-    </View>
+      <OpenApp />
   );
 };
 
 //Function Component
 
 //Export
-export default App;
+export default App;
---
+++ CreativeCircle/source/components/molecules/Bottom/index.js
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Home, Menu, Settings} from '../../../assets';
+
+const BottomTemp = () => {
+  return (
+    <View style={styles.bottom}>
+      <TouchableOpacity>
+        <View style={styles.Home}>
+          <Home />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.Menu}>
+          <Menu />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.Settings}>
+          <Settings />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default BottomTemp;
+
+const styles = StyleSheet.create({
+  container: {},
+  bottom: {
+    flexDirection: 'row',
+    backgroundColor: '#F7F2F2',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Home: {
+    marginRight: 83,
+  },
+  Menu: {
+    marginRight: 83,
+  },
+});
